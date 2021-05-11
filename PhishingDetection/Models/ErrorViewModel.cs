328470490e7b70e3c1c553e3d2030f56c0b85bc9
Sumaya Altamimi <smsomi9@hotmail.com>
2021-05-11 05:38:58 +0300

    create vs project

---
+++ PhishingDetection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhishingDetection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
