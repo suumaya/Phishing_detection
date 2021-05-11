898995bb6bdf5a4e65b8c32bf90b81ffbe9a358c
Sumaya Altamimi <smsomi9@hotmail.com>
2021-05-11 05:41:56 +0300

    net5 instead of net3

--- PhishingDetection/Startup.cs
+++ PhishingDetection/Startup.cs
@@ -30,10 +30,11 @@ namespace PhishingDetection
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,7 @@ namespace PhishingDetection
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
